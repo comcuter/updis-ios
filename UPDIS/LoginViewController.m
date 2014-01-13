5e7ef0d193e9bd3878d30667119818ecc0e51f22
HaiLee <comcuter@gmail.com>
2014-01-14 02:23:12 +0800

    代码整理.

--- UPDIS/LoginViewController.m
+++ UPDIS/LoginViewController.m
@@ -16,16 +16,8 @@
 
 @implementation LoginViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+-(void)dealloc
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
--(void)dealloc{
     TT_RELEASE_SAFELY(_userPanel);
     TT_RELEASE_SAFELY(_userNameBg);
     TT_RELEASE_SAFELY(_userPwdBg);
@@ -41,51 +33,43 @@
     [super dealloc];
 }
 
--(void)viewWillAppear:(BOOL)animated{
+-(void)viewWillAppear:(BOOL)animated
+{
     [super viewWillAppear:animated];
     [[[TTNavigator navigator].topViewController navigationController] setNavigationBarHidden:YES];
 }
 
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
     userLogin = YES;
     TTImageView *bgView = [[TTImageView alloc] init];
     [bgView setUrlPath:(iPhone5? @"bundle://login_bg_5.png": @"bundle://login_bg.png")];
     [self.view insertSubview:bgView atIndex:0];
     TT_RELEASE_SAFELY(bgView);
 
-    //初始化登录窗口
-    //    [[[self userPanel] layer] setBorderColor:[[UIColor blueColor] CGColor]];
-    //    [[[self userPanel] layer] setBorderWidth:1];
-
     [[self userNameBg] setUrlPath:@"bundle://input1.png"];
     [[self userPwdBg] setUrlPath:@"bundle://input1.png"];
 
-
     [self.txtUserName resignFirstResponder];
     [self.txtUserPwd resignFirstResponder];
-
 }
 
-#pragma mark -
-#pragma mark UITextFieldDelegate
-- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
     if ([textField returnKeyType] != UIReturnKeyGo){
         NSInteger nextTag = [textField tag] + 1;
         UIView *nextTextField = [[self view] viewWithTag:nextTag];
         [nextTextField becomeFirstResponder];
-    }
-    else {
+    } else {
         [textField resignFirstResponder];
         [self userLogin];
-
     }
     return YES;
 }
--(void)userLogin{
+
+- (void)userLogin
+{
     NSDictionary *parms = nil;
     if (userLogin) {
         NSString *userName = [self.txtUserName text];
@@ -93,16 +77,17 @@
             TTAlert(@"请输入账号");
             return;
         }
+        
         NSString *userPwd = [BaseFunction md5:[self.txtUserPwd text]];
         if(!userPwd){
             TTAlert(@"请输入密码");
             return;
         }
+        
         parms = [NSDictionary dictionaryWithObjectsAndKeys:userName,@"userName",
                  userPwd,@"pwd",
                  [BaseFunction getMacAddress],@"mac", nil];
-    }
-    else{
+    } else {
         NSString *vCode = [self.txtVCode text];
         if(!vCode){
             TTAlert(@"请输入验证码");
@@ -113,21 +98,20 @@
     NetworkAssistant *networkAssistant =[[NetworkAssistant alloc] init];
     [networkAssistant setDelegate:self];
     [networkAssistant setParameter:parms];
-    [networkAssistant fetchDataFromServer:userLogin?USER_LOGIN:USER_REGPHONE withRootViewController:self];
+    [networkAssistant fetchDataFromServer:userLogin ? USER_LOGIN : USER_REGPHONE withRootViewController:self];
     [networkAssistant release];
 
     [self.txtUserName resignFirstResponder];
     [self.txtUserPwd resignFirstResponder];
     [self.txtVCode resignFirstResponder];
-
-
 }
--(IBAction)userLogin:(id)sender{
+-(IBAction)userLogin:(id)sender
+{
     [self userLogin];
 }
 
--(void)fetchDataOver:(BOOL)succeed operFlag:(NSString *)operFlag{
-
+-(void)fetchDataOver:(BOOL)succeed operFlag:(NSString *)operFlag
+{
     if (!succeed) {
         TTAlert(@"用户认证失败");
         return;
@@ -143,24 +127,18 @@
         if ([[userDic objectForKey:@"success"] integerValue]) {
             //缓存数据
             [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"islogin"];
-            [[NSUserDefaults standardUserDefaults] synchronize];//表示同步保存
+            [[NSUserDefaults standardUserDefaults] synchronize];
             [[AppDelegate sharedAppDelegate] change2RootView];
-        }
-        else{
+        } else {
             TTAlert([userDic objectForKey:@"msg"]);
         }
     }
+    
     if ([operFlag isEqualToString:USER_LOGIN]) {
-     
-        NSString *str = [NSString stringWithContentsOfFile:USER_LOGIN_CACHE_FILE
-                                                  encoding:NSUTF8StringEncoding
-                                                     error:nil];
-
-
+        NSString *str = [NSString stringWithContentsOfFile:USER_LOGIN_CACHE_FILE encoding:NSUTF8StringEncoding error:nil];
         NSDictionary *userDic = [NSJSONSerialization JSONObjectWithData:[str dataUsingEncoding:NSUTF8StringEncoding]
                                                                 options:NSJSONReadingMutableContainers
                                                                   error:nil];
-
         debug_NSLog(@"%@",[userDic objectForKey:@"success"]);
         if ([[userDic objectForKey:@"success"] integerValue]) {
             //登录成功,检查设备
@@ -170,6 +148,7 @@
                 [[AppDelegate sharedAppDelegate] change2RootView];
                 [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"islogin"];
             }
+            
             if(registered==0){
                 //需要注册设备
                 [CoreAnimationEffect animationPushDown:self.userPanel];
@@ -179,24 +158,18 @@
                 userLogin = NO;
                 [self.lblPhoneNum setText:[NSString stringWithFormat:@"您的手机号码为:%@ \n请将短信中的验证码输入下框内",[userDic objectForKey:@"phonenum"]]];
             }
-            if(registered==99){
+            
+            if (registered==99) {
                 //需要填写手机号码
                 TTAlert(@"您没有登记手机号码,请登录内网个人信息修改页面添加手机号码后再登录.");
             }
 
-            [[NSUserDefaults standardUserDefaults] setValue:[userDic objectForKey:@"userid"]
-                                                     forKey:@"userid"];
-
-            [[NSUserDefaults standardUserDefaults] setValue:[BaseFunction md5:[self.txtUserPwd text]]
-                                                     forKey:@"pwd"];
-
+            [[NSUserDefaults standardUserDefaults] setValue:[userDic objectForKey:@"userid"] forKey:@"userid"];
+            [[NSUserDefaults standardUserDefaults] setValue:[BaseFunction md5:[self.txtUserPwd text]] forKey:@"pwd"];
             [[NSUserDefaults standardUserDefaults] setValue:[self.txtUserName text] forKey:@"userName"];
-
-            [[NSUserDefaults standardUserDefaults] setValue:[userDic objectForKey:@"isSpecialUser"]
-                                                     forKey:@"isSpecialUser"];
+            [[NSUserDefaults standardUserDefaults] setValue:[userDic objectForKey:@"isSpecialUser"] forKey:@"isSpecialUser"];
             [[NSUserDefaults standardUserDefaults] synchronize];//表示同步保存
-        }
-        else{
+        } else {
             TTAlert([userDic objectForKey:@"msg"]);
         }
     }
