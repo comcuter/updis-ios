0e011364911b2b81a4bff0bc2f7be191be43f1a7
HaiLee <comcuter@gmail.com>
2014-07-13 17:39:02 +0800

    加入搜索项目负责人的页面.

---
+++ UPDIS/SearchProjectLeadViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchProjectLeadViewController.h
+//  UPDIS
+//
+//  Created by HaiLee on 14-7-13.
+//  Copyright (c) 2014年 UPDIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchProjectLeadViewController : UIViewController
+
+@end
---
+++ UPDIS/SearchProjectLeadViewController.m
@@ -0,0 +1,18 @@
+//
+//  SearchProjectLeadViewController.m
+//  UPDIS
+//
+//  Created by HaiLee on 14-7-13.
+//  Copyright (c) 2014年 UPDIS. All rights reserved.
+//
+
+#import "SearchProjectLeadViewController.h"
+
+@interface SearchProjectLeadViewController ()
+
+@end
+
+@implementation SearchProjectLeadViewController
+
+
+@end
